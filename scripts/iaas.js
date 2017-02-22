f53806609629348036feed5ca4367b1ec218ea63
Erik Barreto de Vera <erikbv99421@gmail.com>
2017-02-22 14:01:50 +0000

    añadido el iaas.js

---
+++ scripts/iaas.js
@@ -0,0 +1,12 @@
+console.log("SSH to iaas.ull");
+
+
+const sshexec = require('ssh-exec');
+
+function puts(error, stdout, stderr) {
+console.log(stdout);
+}
+
+var url_repo = require('../package.json').repository.url_ssh;
+var name_dir = "Practica2";
+sshexec('cd prolen; git clone ${url_repo} ${name_dir}; cd ${name_dir}; node scripts/gitbook.js; npm run server-express', puts);
