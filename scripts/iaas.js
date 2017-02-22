6b8178aa170e51cd044c794926b0ec2e1695717d
Erik Barreto de Vera <erikbv99421@gmail.com>
2017-02-22 15:20:43 +0000

    probando

--- scripts/iaas.js
+++ scripts/iaas.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 console.log("SSH to iaas.ull");
-
-
 const sshexec = require('ssh-exec');
+var exec = require('child_process').exec;
 
 function puts(error, stdout, stderr) {
 console.log(stdout);
@@ -10,4 +9,9 @@ console.log(stdout);
 
 var url_repo = require('../package.json').repository.url_ssh;
 var name_dir = "Practica2";
-sshexec('cd prolen; git clone ${url_repo} ${name_dir}; cd ${name_dir}; npm run build; npm run server-express', puts);
+var host = require('../package.json').host.alias;
+sshexec('git clone git@github.com:ULL-ESIT-PL-1617/primeros-pasos-en-nodejs-erik-jorge-carlos-ruben.git', {
+  user: 'usuario',
+  host: '10.6.128.119'
+}).pipe(process.stdout);
+// ; cd prolen; git clone ${url_repo} ${name_dir}; cd ${name_dir}; npm run build; npm run server-express
