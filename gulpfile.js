3080ca5c867704335c17751ea290066ef8277996
Rubén <alu0100309553@ull.edu.es>
2017-02-22 19:23:07 +0000

    Actualizando servidor

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,12 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('default', ['github', 'build', 'deploy', 'ghpages', 'iaas']);
+gulp.task('default', ['github', 'build', 'deploy', 'iaas']);
 
 gulp.task('github', shell.task(['git add .', 'git commit -m "Actualizando servidor"', 'git push origin master']));
 
 gulp.task('build', shell.task(['npm run build']));
 
 gulp.task('deploy', shell.task(['npm run deploy']));
-
-gulp.task('ghpages', function(){return gulp.src('./gh-pages/**/*').pipe(ghPages)});
 
 gulp.task('iaas', shell.task(['npm run iaas']));
