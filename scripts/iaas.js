ee2f4080831c3e09be239654100f2c1e790aae4c
Erik Barreto de Vera <erikbv99421@gmail.com>
2017-02-23 08:29:52 +0000

    Actualizando servidor

--- scripts/iaas.js
+++ scripts/iaas.js
@@ -9,6 +9,7 @@ console.log(stdout);
 
 sshexec('cd primeros-pasos-en-nodejs-erik-jorge-carlos-ruben && git pull && npm run build', {
   user: 'usuario',
-  host: '10.6.128.156'
+  //host: '10.6.128.156'
+  host: '10.6.128.119'
 }).pipe(process.stdout);
 // ; cd prolen; git clone ${url_repo} ${name_dir}; cd ${name_dir}; npm run build; npm run server-express
